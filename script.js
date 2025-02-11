fd57e02da0a05d1e0b2105cd38d25ded04494967
shatee-ak <abithe99@gmail.com>
2025-02-11 11:40:48 +0600

    test-1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let Age;
+Age = 4;
+if(Age>=60){
+    console.log("Tricket price is $8");
+}
+else if(Age>=18){
+    console.log("Tricket price is $15");
+}
+else if(Age>=5){
+    console.log("Tricket price is $10");
+}
+else{
+    console.log("Tricket Price is Free");
+}
